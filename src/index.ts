94233ac73fc7c39336f9bb89fc2d9ed199114bd9
M307 (Mac) <me@m307.dev>
2023-03-31 18:07:54 +0700

    feat: Init project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+import { CloudflreAccess } from "./access";
+import open from "openurl";
+
+const cf = new CloudflreAccess();
+cf.events.on("login", (url) => {
+    open.open(url)
+    console.log(`Please login via URL:\n\n${url}\n\n`)
+});
+cf.events.on("complete", (token) => console.log(`Login completed! Here your token:\n\n${token?.app_token || token}\n\n`))
+if(!process.argv[2]) throw new Error("Missing URL")
+cf.login(process.argv[2]).then(() => {})
